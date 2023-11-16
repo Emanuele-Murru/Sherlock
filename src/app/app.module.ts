6c41f04b5409b2897949a1a8a2a94a6bade01967
Emanuele <126979214+Emanuele-Murru@users.noreply.github.com>
2023-11-16 03:04:50 +0100

    Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HompageComponent } from './components/hompage/hompage.component';
+
+import { RouterModule, Route } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: HompageComponent
+  },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    HompageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
